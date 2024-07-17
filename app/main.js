63fee1ebe27543c6863e04f59a54e3fd64fd876e
qi.jin <qi.jin@cn.supplyframe.com>
2024-07-17 17:24:14 +0800

    handle set title added.

--- app/main.js
+++ app/main.js
@@ -1,9 +1,12 @@
 const {
     app,
-    BrowserWindow
+    BrowserWindow,
+    ipcMain
 } = require('electron')
 
 const path = require('node:path')
+
+
 
 let mainWindow = null;
 
@@ -20,13 +23,21 @@ const createWindow = () => {
     mainWindow.loadFile(path.join(__dirname, 'index.html'))
 }
 
+const handleSetTitle = (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title);
+}
+
 app.whenReady().then(() => {
+    ipcMain.on('set-title', handleSetTitle)
     createWindow();
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length == 0) {
             createWindow();
         }
     })
+
 });
 
 app.on('window-all-closed', () => {
