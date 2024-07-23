ebe623cd2542b4354bd80b39e3064c77b279253f
qi.jin <qi.jin@cn.supplyframe.com>
2024-07-23 08:44:57 +0800

    remove unregisterAll().

--- app/modules/clipboard.js
+++ app/modules/clipboard.js
@@ -1,114 +1,113 @@
 const {
-    clipboard,
-    nativeImage,
-    Notification,
-    BrowserWindow,
-    globalShortcut
+	clipboard,
+	nativeImage,
+	Notification,
+	BrowserWindow,
+	globalShortcut
 } = require("electron");
 
 const path = require('node:path');
 
 const {
-    updateClipboardTrayMenu
+	updateClipboardTrayMenu
 } = require("./app_tray");
 
 const ClipboardCommon = require("../common");
 
 class ClipBoardWindow {
-    constructor() {
-        this.isShown = false;
-        this.intervals = {};
-        this.clipboardWindow = null;
-        this.assetsPath = path.join(__dirname, '../assets');
-        this.createWindow();
-        this.initClipboardWindowShortcut();
-        this.initWindowEvents();
-        this.initWindowWebContent();
-        this.clippings = ["aaa", 'bbb', 'cc'];
-    }
+	constructor() {
+		this.isShown = false;
+		this.intervals = {};
+		this.clipboardWindow = null;
+		this.assetsPath = path.join(__dirname, '../assets');
+		this.createWindow();
+		this.initClipboardWindowShortcut();
+		this.initWindowEvents();
+		this.initWindowWebContent();
+		this.clippings = ["aaa", 'bbb', 'cc'];
+	}
 
-    createWindow() {
-        const windowOptions = {
-            title: ClipboardCommon.ELECTRON_CLIPBOARD,
-            resizable: true,
-            center: true,
-            show: true,
-            frame: true,
-            autoHideMenuBar: true,
-            icon: path.join(__dirname, 'clipboard.png'),
-            webPreferences: {
-                preload: path.join(this.assetsPath, 'javascript/preload.js'),
-            }
-        };
-        this.clipboardWindow = new BrowserWindow(windowOptions);
-    }
+	createWindow() {
+		const windowOptions = {
+			title: ClipboardCommon.ELECTRON_CLIPBOARD,
+			resizable: true,
+			center: true,
+			show: true,
+			frame: true,
+			autoHideMenuBar: true,
+			icon: path.join(__dirname, 'clipboard.png'),
+			webPreferences: {
+				preload: path.join(this.assetsPath, 'javascript/preload.js'),
+			}
+		};
+		this.clipboardWindow = new BrowserWindow(windowOptions);
+	}
 
-    loadUrl(url) {
-        this.clipboardWindow.loadFile(url);
-    }
+	loadUrl(url) {
+		this.clipboardWindow.loadFile(url);
+	}
 
-    initClipboardWindowShortcut() {
-        this.registerLocalShortcut();
-    }
-    initWindowEvents() {
-        this.clipboardWindow.on('close', (e) => {
-            if (this.clipboardWindow.isVisible) {
-                e.preventDefault();
-                this.hide();
-            }
-            this.unregisterLocalShortcut();
-        });
-        this.clipboardWindow.on('show', () => {
-            this.registerLocalShortcut();
-        });
+	initClipboardWindowShortcut() {
+		this.registerLocalShortcut();
+	}
+	initWindowEvents() {
+		this.clipboardWindow.on('close', (e) => {
+			if (this.clipboardWindow.isVisible) {
+				e.preventDefault();
+				this.hide();
+			}
+			this.unregisterLocalShortcut();
+		});
+		this.clipboardWindow.on('show', () => {
+			this.registerLocalShortcut();
+		});
 
-    }
-    initWindowWebContent() {
-        this.connectClipboard();
-        this.clipboardWindow.webContents.on('dom-ready', () => {
-            // this.clipboardWindow.webContents.insertCSS(CSSInjector.commCSS);
-        });
-    }
+	}
+	initWindowWebContent() {
+		this.connectClipboard();
+		this.clipboardWindow.webContents.on('dom-ready', () => {
+			// this.clipboardWindow.webContents.insertCSS(CSSInjector.commCSS);
+		});
+	}
 
-    connectClipboard() {
-        this.loadUrl(path.join(__dirname, '../', ClipboardCommon.CLIPBOARD))
-    }
+	connectClipboard() {
+		this.loadUrl(path.join(__dirname, '../', ClipboardCommon.CLIPBOARD))
+	}
 
-    toggle() {
-        if(this.isShown) {
-            this.hide();
-        }
-        else {
-            this.show();
-        }
-    }
+	toggle() {
+		if (this.isShown) {
+			this.hide();
+		} else {
+			this.show();
+		}
+	}
 
-    show() {
-        this.clipboardWindow.show();
-        this.clipboardWindow.focus();
-        this.clipboardWindow.webContents.send('show-clipboard-window');
-        this.isShown = true;
-    }
+	show() {
+		this.clipboardWindow.show();
+		this.clipboardWindow.focus();
+		this.clipboardWindow.webContents.send('show-clipboard-window');
+		this.isShown = true;
+	}
 
-    hide() {
-        this.clipboardWindow.hide();
-        this.clipboardWindow.webContents.send('hide-clipboard-window');
-        this.isShown = false;
-    }
+	hide() {
+		this.clipboardWindow.hide();
+		this.clipboardWindow.webContents.send('hide-clipboard-window');
+		this.isShown = false;
+	}
 
-    registerLocalShortcut() {
-        globalShortcut.register('CommandOrControl+H', () => {
-            this.hide();
-            console.log("Global shortcut C+A+H pressed.");
-        });
+	registerLocalShortcut() {
+		globalShortcut.register('CommandOrControl+H', () => {
+			this.hide();
+			console.log("Global shortcut C+A+H pressed.");
+		});
 
-    }
-
+	}
 
 
-    unregisterLocalShortcut() {
-        globalShortcut.unregisterAll();
-    }
+
+	unregisterLocalShortcut() {
+		// globalShortcut.unregisterAll();
+	}
 
 }
 
