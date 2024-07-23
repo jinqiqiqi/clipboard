e2928f1ea0eea9623b418e6d4aefc1e97603cc9e
qi.jin <qi.jin@cn.supplyframe.com>
2024-07-23 15:27:44 +0800

    disable AppConfig

--- app/common.js
+++ app/common.js
@@ -8,46 +8,46 @@ ClipboardCommon.ELECTRON = 'Electron';
 ClipboardCommon.ELECTRON_CLIPBOARD = 'Electron Clipboard';
 ClipboardCommon.DEBUG_MODE = false;
 ClipboardCommon.WINDOW_SIZE = {
-    width: 800,
-    height: 600
+	width: 800,
+	height: 600
 };
 
 ClipboardCommon.CLIPBOARD = 'index.html';
 ClipboardCommon.MENU = {
-    about: 'About Electronic Clipboard',
-    hide: 'Hide Application',
-    hideOther: 'Hide Others',
-    showAll: 'Show All',
-    pref: 'Preference',
-    quit: 'Quit',
-    edit: 'Edit',
-    undo: 'Undo',
-    redo: 'Redo',
-    cut: 'Cut',
-    copy: 'Copy',
-    paste: 'Paste',
-    selectAll: 'Select All',
-    view: 'View',
-    reload: 'Reload This Window',
-    toggleFullScreen: 'Toggle Full Screen',
-    searchContacts: 'Search Contacts',
-    devtool: 'Toggle DevTools',
-    window: 'Window',
-    min: 'Minimize',
-    close: 'Close',
-    allFront: 'Bring All to Front',
-    help: 'Help',
-    repo: 'GitHub Repository',
-    feedback: 'Report Issue',
-    checkRelease: 'Check for New Release',
+	about: 'About Electronic Clipboard',
+	hide: 'Hide Application',
+	hideOther: 'Hide Others',
+	showAll: 'Show All',
+	pref: 'Preference',
+	quit: 'Quit',
+	edit: 'Edit',
+	undo: 'Undo',
+	redo: 'Redo',
+	cut: 'Cut',
+	copy: 'Copy',
+	paste: 'Paste',
+	selectAll: 'Select All',
+	view: 'View',
+	reload: 'Reload This Window',
+	toggleFullScreen: 'Toggle Full Screen',
+	searchContacts: 'Search Contacts',
+	devtool: 'Toggle DevTools',
+	window: 'Window',
+	min: 'Minimize',
+	close: 'Close',
+	allFront: 'Bring All to Front',
+	help: 'Help',
+	repo: 'GitHub Repository',
+	feedback: 'Report Issue',
+	checkRelease: 'Check for New Release',
 };
 
 ClipboardCommon.MSG = (msg, prefix) => {
-    if (!prefix) {
-        console.log(":::::::msg:::::::  => ", msg);
-    } else {
-        console.log(`:::::::(${prefix})::::::: => `, msg);
-    }
+	if (!prefix) {
+		console.log(":::::::msg:::::::  => ", msg);
+	} else {
+		console.log(`:::::::(${prefix})::::::: => `, msg);
+	}
 }
 
 module.exports = ClipboardCommon
